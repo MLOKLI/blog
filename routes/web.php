9530889440b03ba6cf6ea38855ca148f945feea4
MLOKLI <info@mlokli.com>
2018-12-17 13:44:34 +0300

    Create categories

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function() {
   Route::get('/', 'DashboardContoller@dashboard')->name('admin.index');
+  Route::resource('/category', 'CategoryController', ['as'=>'admin']);
 });
 
 Route::get('/', function () {
